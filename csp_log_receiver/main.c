74d2ecc7d7b507c454a973c950abab41e8954a5f
Peter Söderlind <peter.soderlind@gmail.com>
2017-07-21 09:52:37 +0200

    Initial Commit

---
+++ csp_log_receiver/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  csp_log_receiver
+//
+//  Created by peter on 2017-07-21.
+//  Copyright © 2017 peter. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
